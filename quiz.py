99d19e4eab2262bd658845ff13a1c85ae976bcf2
tmlpp <teemu.lappi@gmail.com>
2020-12-02 14:37:33 +0200

    Rename question file and add main python file

---
+++ quiz.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import random
+# hs = [["Teemu", 32], ["Sanna", 12], ["Hanna", 20], ["Foo", 12], ["Katariina", 2], ["Teemu", 32], ["Sanna", 12], ["Hanna", 20], ["Foo", 12], ["Katariina", 2]]
+# names = ["Foo", "Sami", "Katariina", "Jo"]
+
+# for i in range(len(hs)):
+#     print(str(i + 1), hs[i][0], (25 - len(hs[i][0])) * ".", str(hs[i][1]))
+#     # print(hs[i][1] + 25 - len(hs[i][0]) * ".")
+
+# foo = ["Foo", "Bar", "Baz", "Foobar"]
+
+# print(foo)
+
+# foo.insert(2, "Foobaz")
+
+# print(foo)
+
+def print_highscore(arg):
+    pass
+
+def show_highscore(arg):
+    pass
+
+def pick_questions():
+    """Randomly choose 10 uestions from the questions.txt file."""
+
+
+
+
+def main():
+    print("main")
+    pass
+
+if __name__ == '__main__':
+    main()
