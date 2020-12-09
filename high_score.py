faa1a5e743e38c97a8aa9c624995b8bddded5c80
tmlpp <teemu.lappi@gmail.com>
2020-12-09 08:54:53 +0200

    Almost done

---
+++ high_score.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+
+import pickle
+import os
+
+def create_hs_file():
+    """Create the 'high_scores.dat' file if it doest's exist."""
+    try:
+        if not os.path.exists("high_scores.dat"):
+            with open("high_scores.dat", "wb") as hs_file:
+                hs = [[0, "n/a"]] * 10
+                pickle.dump(hs, hs_file)
+            print("High scores file was succesfully created!")
+            return True
+        else:
+            return True
+    except PermissionError:
+        print("Unable to create the high scores file. High score functionality disabled.")
+        return False
+
+
+
+def save_high_score(score, name):
+    """Save the player's result tu high scores list if the result is good enough"""
+
+    # create the file if doesn't exist
+    # if more than 10, pop the last one
+    # if 
+    try:
+        with open("high_scores.dat", "rb") as hs_file:
+            hs = pickle.load(hs_file)
+            print("Alkuperäinen lista: ", hs)
+            print(len(hs))
+            for i in range(len(hs)):
+                if score <= hs[i][0]:
+                    hs.insert(i, [score, name])
+                    break
+                else:
+                    if i == 9:
+                        hs.append([score, name])
+                    else:
+                        continue
+            hs.pop(0)
+            print(hs)
+
+        with open("high_scores.dat", "wb") as hs_file:
+            pickle.dump(hs, hs_file)
+    except PermissionError:
+        print("Unable to write to the high scores file.")
+        
+
+
+def show_high_scores():
+    """Print the high scores from highscores.txt if it exists"""
+    try:
+        with open("high_scores.dat", "rb") as hs_file:
+            hs = pickle.load(hs_file)
+        hs.reverse()
+        print()
+        print("+++++++++++ HIGH SCORES +++++++++++")
+        print("## ... NAME ..................SCORE")
+        for index, hs_line in enumerate(hs, 1):
+            print(index, (5 - len(str(index))) * ".", hs_line[1], (25 - len(str(hs_line[1]))) * ".", hs_line[0])
+    except FileNotFoundError:
+        print("Unable to find the high score file.")
+
+
+if __name__ == '__main__':
+    print("high_score.py is a module for quiz.py and not supposed to be run on its own. Exiting...")
+    
--- quiz.py
+++ quiz.py
@@ -2,62 +2,9 @@
 
 import random
 import sys
+from high_score import *
 
 QUESTIONS = 10
-
-# hs = [["Teemu", 32], ["Sanna", 12], ["Hanna", 20], ["Foo", 12], ["Katariina", 2], ["Teemu", 32], ["Sanna", 12], ["Hanna", 20], ["Foo", 12], ["Katariina", 2]]
-# names = ["Foo", "Sami", "Katariina", "Jo"]
-
-# for i in range(len(hs)):
-#     print(str(i + 1), hs[i][0], (25 - len(hs[i][0])) * ".", str(hs[i][1]))
-#     # print(hs[i][1] + 25 - len(hs[i][0]) * ".")
-
-# foo = ["Foo", "Bar", "Baz", "Foobar"]
-
-# print(foo)
-
-# foo.insert(2, "Foobaz")
-
-# print(foo)
-
-def save_high_score(name, score):
-    """Save the player's result tu high scores list if the result is good enough"""
-
-    # create the file if doesn't exist
-    # if more than 10, pop the last one
-    # if 
-    try:
-        with open("highscores.txt", "r+") as f:
-            highscores = f.readlines()
-            print("dbg: just after reading the file, length of highscores is", str(len(highscores)))
-            highscores.insert(3, name + "\n")
-            print("dbg: after inserting, the length of highscores is", str(len(highscores)))
-            # print(len(highscores))
-            if len(highscores) > 10:
-                highscores.pop()
-            print("dbg: after possible popping the length of highscores is", str(len(highscores)))
-            f.seek(0)
-            f.writelines(highscores)
-    except PermissionError:
-        print("oops")
-        
-
-
-def show_high_scores():
-    """Print the high scores from highscores.txt if it exists"""
-    try:
-        with open("highscores.txt", "r") as f:
-            print("+++ HIGH SCORES +++")
-            print(f.readlines())
-    except FileNotFoundError:
-        print("Can't show the high scores because highscores.txt wasn't found.")
-        print("This probably means no one has played the game before.")
-
-
-def prepare_questions():
-    questions = pick_questions()
-
-
 
 def pick_questions():
     """
@@ -100,6 +47,7 @@ def play_game(arg):
                     break
                 elif answer == 0:
                     print("Game ended. Returning to menu.")
+                    return
                 else:
                     print("Please use numbers 1–4 to answer. Or choose 0 to end the game.")
                     continue
@@ -110,19 +58,19 @@ def play_game(arg):
             print("Unfortunately that's incorrect.")
             print("The correct answer was {}\n".format(questions[i].get(questions[i].get("correct"))))
 
-    save_high_score(score)
+    if hs_enabled == True:
+        save_high_score(score, name)
 
 
 def menu():
     while True:
+        print()
         print("What do you want to do?")
         choice = input("1: Play a new game\n2: Show high score\n0: Quit\n")
         if choice == "1":
             play_game(name)
         elif choice == "2":
             show_high_scores()
-        elif choice == "3":
-            pick_questions()
         elif choice == "0":
             print("Thank you for playing! Bye!")
             sys.exit(0)
@@ -131,8 +79,15 @@ def menu():
 
 
 if __name__ == '__main__':
-    print("Welcome to a quiz game!")
-    name = input("Enter your name: ")
+    hs_enabled = create_hs_file()
+    while True:
+        name = input("Enter your name: ")
+        if len(name) > 20:
+            print("Name can't be over 20 characters")
+            continue
+        else:
+            break
     print()
     print("Hello, " + name + "!")
+    print("Welcome to a quiz game!")
     menu()
